7b353d93150f522914d8c7b5e77a082412c0a1c4
yigitsarpavci <yigitsarpavci@users.noreply.github.com>
2026-05-12 21:45:28 +0000

    deploy: fabf102ed103430b33d784c9e3be50a5d16cad93

--- 2048.js
+++ 2048.js
@@ -12065,8 +12065,8 @@ var _emscripten_stack_get_current = function() {
 
 /** @type {function(...*):?} */
 var ___cxa_is_pointer_type = createExportWrapper("__cxa_is_pointer_type");
-var ___start_em_js = Module['___start_em_js'] = 8927328;
-var ___stop_em_js = Module['___stop_em_js'] = 8927810;
+var ___start_em_js = Module['___start_em_js'] = 8927104;
+var ___stop_em_js = Module['___stop_em_js'] = 8927586;
 function invoke_ii(index,a1) {
   var sp = stackSave();
   try {
